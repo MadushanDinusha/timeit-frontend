ca75417ac12d95812ee02cf1394d90def2ec4e0c
unknown <djabc.pwc@gmail.com>
2022-06-06 19:40:16 +0530

    ss

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const app = express();
 app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile(`./front-end/dist/index.html`, {root: 'dist/angular-app-heroku/'}),
 );
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
