61736d8c0e3eeb56bd81f23729be1d6352a86770
unknown <djabc.pwc@gmail.com>
2022-06-06 19:54:00 +0530

    ss

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('./dist/'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/time-it-app'));
 
-app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/time-it-app/'}),
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
