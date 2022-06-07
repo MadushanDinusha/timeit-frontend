1d47e98d6a925e62f7b825e4b11616c62b34a88c
unknown <djabc.pwc@gmail.com>
2022-06-07 09:34:07 +0530

    time

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -118,8 +118,7 @@ export class HomeComponent implements OnInit {
      if(res.length>0){
       if(this.getTodayTasks1(this.datepipe.transform(res[0].fromDate, 'yyyy-MM-dd'))){
         this.task1Type = res[0].type;
-        var d  = new Date(this.formatDate(res[0].fromDate)).getHours()
-        this.task1StartTime = String(d)
+        this.task1StartTime = String(this.formatDate(res[0].fromDate))
         this.task1EndTime = String(this.formatDate(res[0].toDate))
         console.log(this.task1StartTime +" ss "+this.task1EndTime)
       }}
